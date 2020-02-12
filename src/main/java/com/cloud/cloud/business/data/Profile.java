89b96834e3196e7c2f1a83369f8b50257dd13437
morty <s.m.malekabadi@gmail.com>
2020-02-12 17:25:50 +0330

    bug fix

--- src/main/java/com/cloud/cloud/business/data/Profile.java
+++ src/main/java/com/cloud/cloud/business/data/Profile.java
@@ -17,7 +17,7 @@ public class Profile {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    @Column(unique = true)
+//    @Column(unique = true)
     @NonNull
     private String email;
     private String phoneNo;
